faafd9089534411f61ce7fdaf5a8d0250a4c9137
Tianshun Peng <1677173697@qq.com>
2024-09-14 12:04:54 +0800

    update

--- main.py
+++ main.py
@@ -45,14 +45,11 @@ class MyPlugin(BasePlugin):
         if str(event.event.sender_id) == "1135586980":
             self.ap.logger.info("收到影子的消息")
 
-        original_message = event.event.response_text
-        optimized_message = "回我儿子：" + original_message
-        if optimized_message:
-            event.add_return('reply', optimized_message)
-        
-        self.ap.logger.info(event.__dict__)        
-        self.ap.logger.info(event.event.__dict__)
-            
+            original_message = event.event.response_text
+            optimized_message = "回我儿子：" + original_message
+            if optimized_message:
+                event.add_return('reply', optimized_message)
+
 
     # 插件卸载时触发
     def __del__(self):
